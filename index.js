459c94070d715ca7e39896b7d57e28a14f670617
jamsheer adiyantakath <jamshe.jamsheer@gmail.com>
2025-04-30 20:02:14 +0530

    connected to the mongodb server

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const connectDB = require('./config/db')
 const app = express()
 const port = 3000
+
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 
 
 app.listen(port, () => {
